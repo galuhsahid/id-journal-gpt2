ba08ca17fc7159e1104af938bec020e6391b28bf
Galuh Sahid <galuh.tunggadewi@gmail.com>
2021-08-01 01:11:42 +0700

    Add finetuning script

---
+++ run_finetuning.sh
@@ -0,0 +1,20 @@
+export MODEL_DIR=models
+export TRAIN_DATA="data/train.txt"
+export VAL_DATA="data/val.txt"
+export BASE_MODEL_NAME="flax-community/gpt2-small-indonesian"
+
+python run_clm_flax.py \
+    --num_train_epochs=5 \
+    --output_dir="${MODEL_DIR}" \
+    --model_name_or_path="${BASE_MODEL_NAME}" \
+    --do_train \
+    --train_file="${TRAIN_DATA}" \
+    --do_eval \
+    --validation_file="${VAL_DATA}" \
+    --dataloader_num_workers="64" \
+    --preprocessing_num_workers="64" \
+    --logging_steps="5000" \
+    --save_steps="5000" \
+    --eval_steps="5000" \
+    --overwrite_output_dir \
+    --push_to_hub
